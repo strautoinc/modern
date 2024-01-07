1825e474494a51e609a1901e7562c0d200e27c60
strautoinc <144396023+strautoinc@users.noreply.github.com>
2024-01-07 08:15:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
   var scrollUpBtn = document.getElementById("scrollUpBtn");
 
   window.onscroll = function () {
-      // Show or hide the button based on scroll position
       if (document.body.scrollTop > window.innerHeight || document.documentElement.scrollTop > window.innerHeight) {
           scrollUpBtn.classList.add("visible");
       } else {
@@ -10,22 +9,20 @@ document.addEventListener("DOMContentLoaded", function () {
       }
   };
 
-  // Scroll to the top function
   function scrollToTop() {
-      document.body.scrollTop = 0; // For Safari
-      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+      document.body.scrollTop = 0; 
+      document.documentElement.scrollTop = 0; 
   }
 
-  // Attach the scrollToTop function to the button click event
   scrollUpBtn.addEventListener("click", scrollToTop);
 });
 
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644}, /* Set the initial map center */
-      zoom: 8, /* Set the initial zoom level */
+      center: {lat: -34.397, lng: 150.644}, 
+      zoom: 8, 
       styles: [
-        /* Add your custom map styling here */
+      
         {
           "elementType": "geometry",
           "stylers": [
@@ -34,7 +31,6 @@ function initMap() {
             }
           ]
         },
-        // ... additional styling rules
       ]
     });
   }
